3fb33b47be6d2b8df0dbadbfb58eb76c73cf38d6
mousetailor <anantkushwaha23456@gmail.com>
2025-10-21 18:37:50 +0530

    backend done

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import { ContentModel, UserModel } from "./db.js";
+import { ContentModel, LinkModel, UserModel } from "./db.js";
 import { JWT_PASSWORD } from "./config.js";
 import { userMiddleware } from "./middleware.js";
+import { randomhash } from "./utils.js";
+import cors from "cors"
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
+
 app.post("/api/v1/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -90,13 +93,62 @@ app.delete("/api/v1/content", userMiddleware, async (req, res) => {
 })
 
 
-app.post("/api/v1/brain/share", (req, res) => {
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+  const share = req.body.share;
+  if (share) {
+    const existingLink = await LinkModel.findOne({
+      userId: req.userId
+    })
+    if (existingLink) {
+      res.json({
+        hash: existingLink.hash
+      })
+      return;
+    }
+    const hash = randomhash(10);
+    await LinkModel.create({
+      //@ts-ignore
+      userId: req.userId,
+      hash: hash
+    })
+    res.json({
+      message: "/share" + hash
+    })
+  } else {
+    await LinkModel.deleteOne({
+      //@ts-ignore
+      userId: req.userId
+    });
 
-})
+    res.json({
+      message: "Removed link"
+    })
+  }
 
 
-app.get("/api/v1/brain/:shareLink", (req, res) => {
+  app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
+    const link = await LinkModel.findOne({
+      hash
+    });
+    if (!link) {
+      res.status(411).json({
+        message: "Sorry invalid link"
+      })
+      return
+    }
+    const content = await ContentModel.find({
+      userId: link.userId
+    })
 
-})
+    const user = await UserModel.findOne({
+      _id: link.userId
+    })
 
-app.listen(3000);
+    res.json({
+      username: user?.username,
+      content: content
+    })
+  })
+
+  app.listen(3000);
